a90522b5dd19fd022bbe088f931592c00d833cd8
Chris <christoal@outlook.com>
2018-02-13 17:06:19 -0500

    added table with first 5 results

--- mast_data/views.py
+++ mast_data/views.py
@@ -7,3 +7,13 @@ def lease_list(request):
     leaselist = Masts.objects.all().order_by('current_rent')
     return render(request, 'lease_list.html', {'leaselist': leaselist})
 
+
+def lease_table(request):
+    table = Masts.objects.all().order_by('current_rent')[:5]
+    return render(request, 'lease_table.html', {'table': table})
+
+
+def lease_table_d(request):
+    table = Masts.objects.all().order_by('-current_rent')[:5]
+    return render(request, 'lease_table.html', {'table': table})
+
